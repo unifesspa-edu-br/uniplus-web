8564e9603d4cd1f075cc10232e70358c755ab46b
Denilson Santos Sobrinho Júnior <denilson.santos@unifesspa.edu.br>
2026-05-05 17:02:32 -0300

    test(shared-ui): amplia cobertura de testes para o componente confirm-dialog

--- libs/shared-ui/src/lib/components/confirm-dialog/confirm-dialog.spec.ts
+++ libs/shared-ui/src/lib/components/confirm-dialog/confirm-dialog.spec.ts
@@ -4,11 +4,14 @@ import { describe, expect, it, vi, beforeEach } from 'vitest';
 import { ConfirmDialogComponent } from './confirm-dialog';
 
 describe('ConfirmDialogComponent', () => {
+  const DEFAULT_CONFIRM_LABEL = 'Confirmar';
+  const DEFAULT_CANCEL_LABEL = 'Cancelar';
+
   beforeEach(() => {
     TestBed.configureTestingModule({ imports: [ConfirmDialogComponent] });
   });
 
-  function setup() {
+  function setup(opt?: { confirmLabel?: string, cancelLabel?: string } ) {
     const fixture: ComponentFixture<ConfirmDialogComponent> =
       TestBed.createComponent(ConfirmDialogComponent);
     fixture.detectChanges();
@@ -16,8 +19,8 @@ describe('ConfirmDialogComponent', () => {
     const getDialogEl = () =>
       fixture.debugElement.query(By.css('[role="dialog"]')).nativeElement as HTMLDivElement;
     const getButtons = () => fixture.debugElement.queryAll(By.css('button'));
-    const getCancelButtonEl = () => getButtons().find(b => b.nativeElement.textContent.trim() === 'Cancelar')?.nativeElement as HTMLButtonElement;
-    const getConfirmButtonEl = () => getButtons().find(b => b.nativeElement.textContent.trim() === 'Confirmar')?.nativeElement as HTMLButtonElement;
+    const getCancelButtonEl = () => getButtons().find(b => b.nativeElement.textContent.trim() === (opt?.cancelLabel ?? DEFAULT_CANCEL_LABEL))?.nativeElement as HTMLButtonElement;
+    const getConfirmButtonEl = () => getButtons().find(b => b.nativeElement.textContent.trim() === (opt?.confirmLabel ?? DEFAULT_CONFIRM_LABEL))?.nativeElement as HTMLButtonElement;
     const confirmedOutputSpy = vi.spyOn(component.confirmed, 'emit');
     const cancelledOutputSpy = vi.spyOn(component.cancelled, 'emit');
     return {
@@ -84,5 +87,79 @@ describe('ConfirmDialogComponent', () => {
 
     expect(component.visible()).toBe(false);
     expect(confirmedOutputSpy).toHaveBeenCalled();
+  });
+
+  it('renderiza um título customizado', () => {
+    const { fixture, component } = setup();
+    fixture.componentRef.setInput('visible', true);
+    const title = 'Tela de Confirmação';
+    fixture.componentRef.setInput('title', title);
+    fixture.detectChanges();
+
+    const headingEl = fixture.debugElement.query(By.css('h3')).nativeElement as HTMLHeadingElement;
+
+    expect(component.title()).toBe(title);
+    expect(headingEl.textContent?.trim()).toBe(component.title());
+  });
+
+  it('renderiza um parágrafo customizado', () => {
+    const { fixture, component } = setup();
+    fixture.componentRef.setInput('visible', true);
+    const message = 'Deseja realmente prosseguir com a operação?';
+    fixture.componentRef.setInput('message', message);
+    fixture.detectChanges();
+
+    const paragraphEl = fixture.debugElement.query(By.css('p')).nativeElement as HTMLParagraphElement;
+
+    expect(component.message()).toBe(message);
+    expect(paragraphEl.textContent?.trim()).toBe(component.message());
+  });
+
+  it('renderiza botão de confirmação com um texto customizado', () => {
+    const confirmLabel = 'Confirmar Operação';
+    const { fixture, component, getConfirmButtonEl } = setup({ confirmLabel: confirmLabel });
+    fixture.componentRef.setInput('visible', true);
+    fixture.componentRef.setInput('confirmLabel', confirmLabel);
+    fixture.detectChanges();
+
+    const confirmButtonEl = getConfirmButtonEl();
+
+    expect(confirmButtonEl.textContent?.trim()).toBe(confirmLabel);
+    expect(confirmButtonEl.textContent?.trim()).toBe(component.confirmLabel());
+  });
+
+  it('renderiza botão de cancelar com texto customizado', () => {
+    const cancelLabel = 'Cancelar Operação';
+    const { fixture, getCancelButtonEl } = setup({ cancelLabel: cancelLabel });
+    fixture.componentRef.setInput('visible', true);
+    fixture.componentRef.setInput('cancelLabel', cancelLabel);
+    fixture.detectChanges();
+
+    const cancelButtonEl = getCancelButtonEl();
+
+    expect(cancelButtonEl.textContent?.trim()).toBe(cancelLabel);
+  });
+
+  it('aplica classes css no botão de confirmação quando o input confirmVariant() é "primary" (default)', () => {
+    const { fixture, getConfirmButtonEl } = setup();
+    fixture.componentRef.setInput('visible', true);
+    fixture.detectChanges();
+
+    const confirmButtonEl = getConfirmButtonEl();
+
+    expect(confirmButtonEl.classList).toContain('bg-unifesspa-primary');
+    expect(confirmButtonEl.classList).toContain('hover:bg-blue-800');
+  });
+
+  it('aplica classes css no botão de confirmação quando o input confirmVariant() é "danger"', () => {
+    const { fixture, getConfirmButtonEl } = setup();
+    fixture.componentRef.setInput('visible', true);
+    fixture.componentRef.setInput('confirmVariant', 'danger');
+    fixture.detectChanges();
+
+    const confirmButtonEl = getConfirmButtonEl();
+
+    expect(confirmButtonEl.classList).toContain('bg-red-600');
+    expect(confirmButtonEl.classList).toContain('hover:bg-red-700');
   });
 });
