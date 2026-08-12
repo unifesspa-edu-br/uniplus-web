5a0fca8eb17c92a1d8cefbe208c22ee5e9c7639f
Denilson Santos Sobrinho Júnior <denilson.santos@unifesspa.edu.br>
2026-08-12 10:46:16 -0300

    fix(shared-ui): adiciona entrypoint de pipes no módulo shared-ui

---
+++ libs/shared-ui/pipes/src/index.ts
@@ -0,0 +1,4 @@
+export { CpfPipe } from '../../src/lib/pipes/cpf.pipe';
+export { DateBrPipe } from '../../src/lib/pipes/date-br.pipe';
+export { NomeSocialPipe } from '../../src/lib/pipes/nome-social.pipe';
+
