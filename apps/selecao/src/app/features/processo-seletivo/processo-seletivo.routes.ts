220976fde103dd743b8c58ac553b06fce3ae6d18
NalandaMelo <nalandamelo28@gmail.com>
2026-08-19 14:42:24 -0300

    feat(selecao):lista, retomar e hidratar Processos Seletivos

--- apps/selecao/src/app/features/processo-seletivo/processo-seletivo.routes.ts
+++ apps/selecao/src/app/features/processo-seletivo/processo-seletivo.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from '../dashboard/dashboard.page';
 import { ProcessoSeletivoPage } from './processo-seletivo.page';
 
 export const PROCESSO_SELETIVO_ROUTES: Routes = [
   {
     path: '',
+    component: DashboardPage,
+    data: {
+      breadcrumb: 'Processo Seletivo',
+    },
+  },
+  {
+    path: 'novo',
+    component: ProcessoSeletivoPage,
+    data: {
+      breadcrumb: 'Novo Processo Seletivo',
+    },
+  },
+  {
+    path: ':id',
     component: ProcessoSeletivoPage,
     data: {
       breadcrumb: 'Processo Seletivo',
